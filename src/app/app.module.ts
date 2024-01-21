ba1dc9eb135c33157a978b3f3d307eb9f6e88f9b
Juan Rioseco Z <jrioseco@emeal.nttdata.com>
2024-01-20 22:36:25 -0300

    add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CountriesModule } from './countries/countries.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    CountriesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
